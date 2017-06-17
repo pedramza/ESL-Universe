de2065743e03c68de12127c6c26f4072d6b2f919
Pedram Zadno Azizi <pedram.azizi@gmail.com>
2017-06-17 22:54:00 +1000

    Initial commit

---
+++ ESL.App/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ESL.App
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			using (var s = new System.Speech.Synthesis.SpeechSynthesizer())
+			{
+				s.SetOutputToDefaultAudioDevice();
+				s.Speak("Welcome to E S L universe.");
+				s.Speak("all english learning tools for anyone who speaks english as a second language in a single package.");
+			}
+		}
+	}
+}
